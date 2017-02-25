2ec0017f80cf2648878574fe66758a2920770026
Charleen D. Adams <charleen.adams@bristol.ac.uk>
2017-02-25 21:17:15 +0000

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,4 +1,4 @@
 ui = basicPage(
-  h2('ProtecT Results Associating rostate Cancer Incidence with Metabolites'),
+  h2('ProtecT Results Associating Prostate Cancer Incidence with Metabolites'),
   dataTableOutput('mytable')
 )
